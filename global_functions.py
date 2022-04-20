a585ed14daec73ab373d30923e1bbc730de70a2b
Usuariozombie <edupime_98@hotmail.com>
2022-04-20 14:22:26 +0200

    V1.6 Update (minor changes)

--- global_functions.py
+++ global_functions.py
@@ -1,31 +1,19 @@
 import json
 
 ban_msg = [
-    "flew to close to the radar and got banned",
-    "messed up bad and got banned",
-    "has been struck by the BAN HAMMER",
-    "annoyed some staff and got banned",
-    "wanted to see what would happen if you broke rules and got banned",
-    "tried to dodge the ban hammer :rofl:",
-    "was blown up by Creeper",
-    "was killed by [Intentional Game Design]",
-    "tried to swim in lava",
-    "experienced kinetic energy",
-    "drowned",
-    "hit the ground too hard",
-    "was squashed by a falling anvil",
-    "was squished too much",
-    "fell out of the world",
-    "went up in flames",
-    "went off with a bang",
-    "was struck by lightning",
-    "discovered the floor was lava",
+    "Tried to swim in lava.",
+    "No server?",
+    "poggers are not allowed in this server.",
+    "KONO DIO DA!",
+    "Oh! Mukatte kuru no ka?"
 ]
 
 kick_msg = [
-    "got booted and got kicked?",
-    "got kicked, imagine getting kicked...",
-    "got kicked... I ran out of jokes",
+    "flew to close to the radar and got kicked.",
+    "messed up bad and got kicked, lol.",
+    "has been struck by the HAMMER.",
+    "annoyed some staff and got kicked.",
+    "wanted to see what would happen if you broke rules and got kicked.",
 ]
 
 EMOJIS_TO_USE_FOR_CALCULATOR = {"1":"1️⃣", "2":"2️⃣", "3":"3️⃣", "4":"4️⃣", "5":"5️⃣", "6":"6️⃣", "7":"7️⃣", "8":"8️⃣", "9":"9️⃣", "0":"0️⃣", "+":"➕", "-":"➖","x":"✖️","÷":"➗",".":"⚫"}
